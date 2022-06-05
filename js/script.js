93936fe882e447b337248c67475fd61a237c7e21
Astaldo <astaldo07@mail.ru>
2022-06-06 00:15:56 +0300

    update slider

--- js/script.js
+++ js/script.js
@@ -18,24 +18,27 @@ $(document).ready(function() {
 
     responsive: [
     {
-        breakpoint: 1200,
+        breakpoint: 1900,
         settings: {
             slidesToShow: 5,
-            slidesToScroll: 1,
+        }
+    },
+    {
+        breakpoint: 1280,
+        settings: {
+            slidesToShow: 3,
         }
     },
     {
         breakpoint: 1024,
         settings: {
             slidesToShow: 3,
-            slidesToScroll: 1,
         }
     },
     {
         breakpoint: 800,
         settings: {
             slidesToShow: 2,
-            slidesToScroll: 1,
         }
     }
 
